69f120898a636ddd019c328b2e175130280760b6
Pablo Arauz Caballero <128090351+PabloArauzCaballero@users.noreply.github.com>
2026-07-04 21:10:08 -0400

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
@@ -45,6 +45,6 @@ import { AuditModule } from './modules/audit/audit.module';
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer): void {
-    consumer.apply(RequestIdMiddleware).forRoutes('*');
+    consumer.apply(RequestIdMiddleware).forRoutes({ path: '/{*path}', method: RequestMethod.ALL });
   }
 }
